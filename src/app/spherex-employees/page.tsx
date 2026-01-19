e5072dda99b9668813612bd97aa2763137e26a70
WISDOM-WOKE22 <wisdomwokedev@gmail.com>
2026-01-19 18:10:22 +0100

    feat: Wrap SpherexEmployeesPage in Suspense with Skeleton fallback to improve loading state handling during data fetching, enhancing user experience.

--- src/app/spherex-employees/page.tsx
+++ src/app/spherex-employees/page.tsx
@@ -2,12 +2,16 @@
 
 import DashboardLayout from "@/core/commons/layout/dashboardLayout";
 import { SpherexEmployeeLayout } from "@/modules/spherex-employees/layouts";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const SpherexEmployeesPage = () => {
     return (
-        <DashboardLayout pageTitle="Spherex Employees">
-            <SpherexEmployeeLayout />
-        </DashboardLayout>
+        <Suspense fallback={<Skeleton className="h-screen w-full" />}>
+            <DashboardLayout pageTitle="Spherex Employees">
+                <SpherexEmployeeLayout />
+            </DashboardLayout>
+        </Suspense>
     )
 }
 
