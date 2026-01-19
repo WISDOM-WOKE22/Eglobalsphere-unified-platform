0b7cce45e7bf75e2ad8633cf986cb19abf67a7de
WISDOM-WOKE22 <wisdomwokedev@gmail.com>
2026-01-19 17:57:00 +0100

    feat: Integrate loading states in Spherex employee and visitor components using Suspense and Skeleton, enhancing user experience during data fetching.

--- src/app/spherex-employees/[id]/page.tsx
+++ src/app/spherex-employees/[id]/page.tsx
@@ -3,15 +3,19 @@
 import DashboardLayout from "@/core/commons/layout/dashboardLayout"
 import { EmployeeLayout } from "@/modules/spherex-employees/layouts/user"
 import { useStore } from "@/lib/zustand/store"
+import { Suspense } from "react"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function SpherexEmployee() {
     const spherexEmployee = useStore((state) => state.spherexEmployee);
     return (
+        <Suspense fallback={<Skeleton className="h-screen w-full" />}>  
         <DashboardLayout
             pageTitle={spherexEmployee?.fullname || 'Spherex Employee'}
             subHeading={spherexEmployee?.fullname || 'Spherex Employee'}
-        >
-            <EmployeeLayout />
-        </DashboardLayout>
+            >
+                <EmployeeLayout />
+            </DashboardLayout>
+        </Suspense>
     )
 }
--- src/app/spherex-visitors/[id]/page.tsx
+++ src/app/spherex-visitors/[id]/page.tsx
@@ -1,13 +1,21 @@
+"use client"
+
 import DashboardLayout from "@/core/commons/layout/dashboardLayout"
 import { EmployeeLayout } from "@/modules/spherex-employees/layouts/user"
+import { Suspense } from "react"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useStore } from "@/lib/zustand/store"
 
 export default function SpherexVisitor() {
+    const spherexVisitor = useStore((state) => state.spherexVisitor);
     return (
-        <DashboardLayout
-            pageTitle="Wisdmo Woke"
-            subHeading="wisdom woke"
+        <Suspense fallback={<Skeleton className="h-screen w-full" />}> 
+         <DashboardLayout
+            pageTitle={spherexVisitor?.fullname || 'Spherex Visitor'}
+            subHeading={spherexVisitor?.fullname || 'Spherex Visitor'}
         >
             <EmployeeLayout />
         </DashboardLayout>
+        </Suspense>
     )
 }
